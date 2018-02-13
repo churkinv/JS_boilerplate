7282e87c3a22e169f5f76ad70634e95ac1ca0a30
Churkin Valeriy <v.churkin@coworking.local>
2018-02-13 15:46:47 +0200

    - pixi.js installe - some drafts for prod depl. added

---
+++ src/game.js
@@ -0,0 +1,12 @@
+// import { PIXI } from 'pixi.js';
+
+// // The application will create a renderer using WebGL, if possible,
+// // with a fallback to a canvas render. It will also setup the ticker
+// // and the root stage PIXI.Container
+// const app = new PIXI.Application();
+
+// //Aliases
+// let Application = PIXI.Application,
+//     loader = PIXI.loader,
+//     resources = PIXI.loader.resources,
+//     Sprite = PIXI.Sprite;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,28 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+
 
 export default {
   debug: true,
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   noInfo: false,
   entry: [
     path.resolve(__dirname, 'src/main')
   ],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
   plugins: [
+    // Mimfy JS
+    new webpack.optimize.UglifyJsPlugin(),
+
+    //Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
@@ -23,8 +31,8 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.css$/, loaders: ['style', 'css'] }
     ]
   }
 }
